88eecd1d1f10e860714ec204b9dcd73fe3b2c974
oricardos <ricardo.rs95@gmail.com>
2025-10-15 20:24:30 -0300

    criando metodos de listar e salvar

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const LINKS_STORAGE_KEY = 'links_storage';
+
+type LinkStorage = {
+    id: string
+    name: string
+    url: string
+    category: string
+}
+
+async function get(): Promise<LinkStorage[]> {
+    const storage = await AsyncStorage.getItem(LINKS_STORAGE_KEY)
+    const response = storage ? JSON.parse(storage) : [];
+    return response
+}
+
+async function save(newLink: LinkStorage) {
+    try {
+        const storage = await get();
+        await AsyncStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify([...storage, newLink]))
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const linkStorage = { get, save }
